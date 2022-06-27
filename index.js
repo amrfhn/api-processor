8e7bb1d9028b99d98e9ceaabfb318446ab16d7ef
Amir Farhan bin Jefri <amirfarhan.jfri@gmail.com>
2022-06-28 01:20:17 +0800

    add env and port

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
-const express = require('express');
-const Datastore = require('nedb');
-const fetch = require('node-fetch');
+const express = require("express");
+const Datastore = require("nedb");
+const fetch = require("node-fetch");
 
 const app = express();
-app.listen(3000, () => console.log('listening at 3000'));
-app.use(express.static('public'));
-app.use(express.json({ limit: '1mb' }));
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`listening at ${port}`));
+app.use(express.static("public"));
+app.use(express.json({ limit: "1mb" }));
 
-const database = new Datastore('database.db');
+const database = new Datastore("database.db");
 database.loadDatabase();
 
-app.get('/api/rsvp', (request, response) => {
+app.get("/api/rsvp/:type", (request, response) => {
+  const type = request.params.type.split(",");
+  const guestType = type[0];
+  const isAttending = type[1];
   database.find({}, (err, data) => {
     if (err) {
       response.end();
@@ -20,15 +24,15 @@ app.get('/api/rsvp', (request, response) => {
   });
 });
 
-app.post('/api/rsvp', (request, response) => {
+app.post("/api/rsvp", (request, response) => {
   const data = request.body;
   const timestamp = Date.now();
   data.timestamp = timestamp;
   database.insert(data);
   response.json({
-    status: 'success',
+    status: "success",
     name: data.Name,
-    timestamp: timestamp
+    timestamp: timestamp,
   });
-  console.log(data)
-})
+  console.log(data);
+});
