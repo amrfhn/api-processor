c93d03e196045a52febe0b198a46bd7d1d550a4d
Amir Farhan bin Jefri <amirfarhan.jfri@gmail.com>
2022-06-28 23:20:13 +0800

    fix bug

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const Datastore = require("nedb");
-const fetch = require("node-fetch");
+// const fetch = require("node-fetch");
+require("dotenv").config();
 
 const app = express();
 const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`listening at ${port}`));
+app.listen(port, () => {
+  console.log(`Starting server at ${port}`);
+});
 app.use(express.static("public"));
 app.use(express.json({ limit: "1mb" }));
 
@@ -26,10 +29,41 @@ app.post("/api/rsvp", (request, response) => {
   const timestamp = Date.now();
   data.timestamp = timestamp;
   database.insert(data);
-  response.json({
-    status: "success",
-    name: data.Name,
-    timestamp: timestamp,
-  });
-  console.log(data);
+  response.json(data);
 });
+
+// const express = require("express");
+// const Datastore = require("nedb");
+// // const fetch = require("node-fetch");
+
+// const app = express();
+// const port = process.env.PORT || 3000;
+// app.listen(port, () => console.log(`listening at ${port}`));
+// app.use(express.static("public"));
+// app.use(express.json({ limit: "1mb" }));
+
+// const database = new Datastore("database.db");
+// database.loadDatabase();
+
+// app.get("/api/rsvp", (request, response) => {
+//   database.find({}, (err, data) => {
+//     if (err) {
+//       response.end();
+//       return;
+//     }
+//     response.json(data);
+//   });
+// });
+
+// app.post("/api/rsvp", (request, response) => {
+//   const data = request.body;
+//   const timestamp = Date.now();
+//   data.timestamp = timestamp;
+//   database.insert(data);
+//   response.json({
+//     status: "success",
+//     name: data.Name,
+//     timestamp: timestamp,
+//   });
+//   console.log(data);
+// });
